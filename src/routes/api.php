a384a82a2b4240daf2d73b2e7aba35aabfce4e87
Naud14 <n.drost14@gmail.com>
2024-06-02 15:54:49 +0200

    VanToVan API
    Implement function to move products from van to van with API

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,4 +3,10 @@
 use App\Http\Controllers\VanController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/van/product/move', [VanController::class, 'moveProductToVan']);
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::post('/van/product/move', [VanController::class, 'moveProductToVanAPI']);
+Route::post('/warehouse/product/move', [VanController::class, 'moveProductToWarehouseAPI']);
+Route::post('/van/product/van/move', [VanController::class, 'moveProductFromVanToVanAPI']);
