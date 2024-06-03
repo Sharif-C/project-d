d56307d44c64423d608554bcfe3b0717e66cd552
JurDev <jurwinevertsz@live.nl>
2024-06-03 10:24:46 +0200

    fix: Removed Action.php/ removed unused lines api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\VanController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
-
 Route::post('/van/product/move', [VanController::class, 'moveProductToVan']);
